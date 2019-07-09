6f00c45adf80da7b30a854539d435cad685a060d
Jun Kim <93kimhyunjun@gmail.com>
2019-07-09 23:43:37 +0900

    Add to postfix logic

--- src/index.js
+++ src/index.js
@@ -1,31 +1,38 @@
 const output = document.getElementById("output");
 const result = document.getElementById("result");
 const clear = document.getElementById("clear");
-const arrValues = [];
+const OriginalArrValues = [];
+const postfixValues = [];
+const stackValues = [];
 
 // 버튼 입력 값을 읽고 배열에 저장합니다.
 const readValue = event => {
   // console.log(event.target.innerHTML); v
   const value = event.target.innerHTML;
   if (value === "CE") {
-    arrValues.pop();
+    OriginalArrValues.pop();
     displayValue();
   } else if (value === "AC") {
     clearAll();
     clear.innerHTML = "CE";
   } else {
-    arrValues.push(value);
+    OriginalArrValues.push(value);
     displayValue();
   }
-  console.log(arrValues);
+  if (OriginalArrValues.length !== 0) {
+    clear.innerHTML = "CE";
+  } else {
+    clear.innerHTML = "AC";
+  }
+  console.log(OriginalArrValues);
 };
 
 // output의 innerHTML 값을 배열을 누산한 값으로 리턴합니다.
 const displayValue = () => {
-  if (arrValues.length === 0) {
+  if (OriginalArrValues.length === 0) {
     output.innerHTML = 0;
   } else {
-    output.innerHTML = arrValues.reduce((prev, curr) => {
+    output.innerHTML = OriginalArrValues.reduce((prev, curr) => {
       return prev + curr;
     });
   }
@@ -39,6 +46,8 @@ const spliceValues = arr => {
       arr[i] === "-" ||
       arr[i] === "X" ||
       arr[i] === "/" ||
+      arr[i] === "(" ||
+      arr[i] === ")" ||
       arr[i] === "="
     ) {
       const num = arr.splice(0, i);
@@ -78,89 +87,99 @@ const calculateValues = (currentNum, op, nextNum) => {
 };
 
 const clearAll = () => {
-  if (arrValues.length !== 0) {
-    while (arrValues.length !== 0) arrValues.pop();
+  if (OriginalArrValues.length !== 0) {
+    while (OriginalArrValues.length !== 0) OriginalArrValues.pop();
   }
   output.innerHTML = 0;
 };
 
-const accumulateValues = value => {
-  arrValues.pop();
-  arrValues.push(value);
-};
-
 // 배열의 길이가 1인지 'true' or 'false'로 체크합니다.
-const checkArrValues = () => arrValues.length === 1;
-
-// 배열에 괄호가 있는지 확인합니다.
-const checkBracket = () => {
-  return arrValues.indexOf("(") !== -1 && arrValues.indexOf(")") !== -1;
-};
-
-// 괄호 부분을 따로 분리합니다.
-const spliceBracket = arr => {
-  const openBracket = arrValues.indexOf("(");
-  const closeBracket = arrValues.indexOf(")");
-  console.log(openBracket, closeBracket);
-  return arr.slice(openBracket, closeBracket + 1);
-};
-
-// 괄호를 제거합니다.
-const removeBracket = spliceBracketValues => {
-  spliceBracketValues.shift();
-  const closeBracket = spliceBracketValues.indexOf(")");
-  spliceBracketValues.splice(closeBracket, 1);
-  return spliceBracketValues;
-};
+const checkOriginalArrValues = () => OriginalArrValues.length === 1;
 
 // 배열을 연산자 기준으로 분리 후 다시 합쳐 데이터 형식을 숫자로 바꿉니다.
 const numValueLoop = () => {
-  const splice = spliceValues(arrValues);
-  // console.log(splice); v
-  const join = joinValues(splice);
-  // console.log(join); v
-  const currentNum = changeTypeToNumber(join);
-  // console.log(currentNum); v
-  return currentNum;
-};
-
-const priorityValueLoop = () => {
-  const splice = spliceBracket(arrValues);
+  const splice = spliceValues(OriginalArrValues);
   // console.log(splice);
-  const strNumber = removeBracket(splice);
-  // console.log(strNumber);
-  const join = joinValues(strNumber);
+  const join = joinValues(splice);
   // console.log(join);
   const currentNum = changeTypeToNumber(join);
-  return currentNum;
+  // console.log(currentNum);
+  postfixValues.push(currentNum);
 };
 
-const returnNum = () => {
-  return checkBracket() ? priorityValueLoop() : numValueLoop();
-};
-
-const runCalculationLoop = num => {
-  while (!checkArrValues()) {
-    const op = arrValues.shift();
-    const nextNum = numValueLoop();
-    num = calculateValues(num, op, nextNum);
+const getOperator = () => {
+  const op = OriginalArrValues.shift();
+  const lastValue = stackValues[stackValues.length - 1];
+  if (stackValues.length === 0) {
+    // console.log(op);
+    stackValues.push(op);
+  } else if (
+    (op === "X" || op === "/") &&
+    (lastValue === "+" || lastValue === "-")
+  ) {
+    // console.log(op);
+    stackValues.push(op);
+  } else {
+    // console.log(op);
+    const postfixOp = stackValues.pop();
+    stackValues.push(op);
+    postfixValues.push(postfixOp);
   }
-  return num;
+};
+
+const pushLeftOperator = () => {
+  while (!(stackValues.length === 0)) {
+    const postfixOp = stackValues.pop();
+    postfixValues.push(postfixOp);
+  }
 };
 
 // 로직 실행
 const action = () => {
-  let currentNum = numValueLoop();
-  currentNum = runCalculationLoop(currentNum);
-  accumulateValues(currentNum);
+  while (!checkOriginalArrValues()) {
+    numValueLoop();
+    if (OriginalArrValues[0] !== "=") {
+      getOperator();
+    }
+  }
+  console.log(stackValues);
+  pushLeftOperator();
+  // console.log(postfixValues); 정상작동
+  while (postfixValues.length !== 0) {
+    if (typeof postfixValues[0] === "number") {
+      let testA = postfixValues.shift();
+      stackValues.push(testA); //잘됨
+    }
+    if (typeof postfixValues[0] !== "number") {
+      let op = postfixValues.shift();
+      let num1 = stackValues.pop();
+      let num2 = stackValues.pop();
+      let result = calculateValues(num1, op, num2);
+      // console.log(num1, num2, result);
+      stackValues.push(result);
+    }
+  }
+  currentNum = test();
+  // console.log(postfixValues);
   return currentNum;
+};
+
+const test = () => {
+  const result = stackValues.shift();
+  OriginalArrValues.pop();
+  OriginalArrValues.push(result);
+  return result;
 };
 
 // '=' 을 입력했을 때 메인 액션을 실행하고 화면에 출력합니다.
 const execLogic = event => {
   const value = event.target.innerHTML;
-  arrValues.push(value);
+  OriginalArrValues.push(value);
   output.innerHTML = action();
   result.innerHTML = `Ans = ${output.innerHTML}`;
   clear.innerHTML = "AC";
 };
+
+// 맨 앞에 -와 괄호만 삽입 가능
+// 괄호 로직 처리
+// 괄호 로직 처리를 위해서 -와 괄호 오는 경우에 대한 처리 로직 필요
